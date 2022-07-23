bbc0822d0b44775e777b73eeb14533215be60237
Drew Marshall <thedrewguy@gmail.com>
2022-07-23 15:43:18 -0700

    Initial commit, no UI yet, a few actions and reducers added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,30 @@ import App from './App'
 
 import './api/server'
 
+import store from './store'
+
+console.log('Initial state: ', store.getState())
+
+const unsubscribe = store.subscribe(() =>
+  console.log('State after dispatch: ', store.getState())
+)
+
+store.dispatch({ type: 'todos/todoAdded', payload: 'Learn React' })
+store.dispatch({ type: 'todos/todoAdded', payload: 'Learn Redux' })
+store.dispatch({ type: 'todos/todoAdded', payload: 'Build Stuff' })
+store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
+
+store.dispatch({ type: 'todos/todoToggled', payload: 1 })
+
+store.dispatch({ type: 'filters/statusFilterChanged', payload: 'Completed' })
+
+store.dispatch({
+  type: 'filters/colorFilterChanged',
+  payload: { changeType: 'added', color: 'green' },
+})
+
+unsubscribe()
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
