f0f7f39d14bbe9d955453648565abc7558ee5cd7
Drew Marshall <thedrewguy@gmail.com>
2022-07-23 16:37:54 -0700

    Middleware
    Middleware are basically chainable functions that wrap the dispatch function.
    Chain them using redux.applyMiddleware, which creates an enhancer which you add to your store
    this example has an anti-green middleware which rejects green filtering actions.
    Useful for logging, implementing side effects, etc...
    good for keeping async code outside of the reducers which should be pure functions

--- src/exampleAddons/middleware.js
+++ src/exampleAddons/middleware.js
@@ -3,12 +3,21 @@ export const print1 = (storeAPI) => (next) => (action) => {
   return next(action)
 }
 
-export const print2 = (storeAPI) => (next) => (action) => {
-  console.log('2')
-  return next(action)
-}
-
-export const print3 = (storeAPI) => (next) => (action) => {
-  console.log('3')
-  return next(action)
+export const antiGreenMiddleware = function (storeAPI) {
+  return function wrapDispatch(next) {
+    console.log('AGM - Wrapping dispatch - happens once')
+    return function handleAction(action) {
+      console.log('AGM - handling action')
+      if (
+        action.action.type === 'filters/colorFilterChanged' &&
+        action.action.payload.color === 'green'
+      ) {
+        console.log('AGM - no green allowed. The buck stops here')
+        return
+      } else {
+        console.log('AGM - action allowed')
+        return next(action)
+      }
+    }
+  }
 }
--- src/store.js
+++ src/store.js
@@ -1,13 +1,16 @@
-import { compose, createStore } from 'redux'
+import { applyMiddleware, compose, createStore } from 'redux'
 import { sayHiOnDispatch } from './exampleAddons/enhancers'
+import { antiGreenMiddleware, print1 } from './exampleAddons/middleware'
 import rootReducer from './reducer'
 
+// enhancers
 const devToolsEnhancer =
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-
 const sayHiEnhancer = sayHiOnDispatch
 
-const composedEnhancer = compose(devToolsEnhancer, sayHiEnhancer)
+const mwEnhancer = applyMiddleware(antiGreenMiddleware, print1)
+
+const composedEnhancer = compose(devToolsEnhancer, sayHiEnhancer, mwEnhancer)
 
 const store = createStore(rootReducer, undefined, composedEnhancer)
 
